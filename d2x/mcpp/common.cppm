4017b38df9c78089c1ab6a7bc69fefe21886f60d
FrozenlemonTee <1115306170@qq.com>
2026-02-04 22:40:09 +0800

    refactor: Remove files

--- d2x/mcpp/common.cppm
+++
@@ -1,5 +0,0 @@
-module;
-
-#include <d2x/cpp/common.hpp>
-
-export module d2x.project.common;
--- dslings/xmake.lua
+++
@@ -1,9 +0,0 @@
-target("hello-mcpp")
-    set_languages("c++23")
-    add_files("hello-mcpp.cpp")
-    add_files("../d2x/mcpp/common.cppm")
-
-target("helloworld")
-    add_files("helloworld.cpp")
-
-
