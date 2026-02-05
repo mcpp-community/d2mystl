5a4dec07462f0427febfe4e910f9cf1a179313dd
sunrisepeak <speakshen@163.com>
2026-02-05 18:14:23 +0800

    tmp

--- src/chapter0/xmake.lua
+++ src/chapter0/xmake.lua
@@ -1,11 +1,4 @@
 -- Chapter 0: MetaInfo module
 target("chapter0_modules")
-    set_kind("static")
-    set_languages("c++23")
-    
-    set_policy("build.c++.modules", true)
-    
-    add_files("global.cppm")
+    add_files("global.cppm", { public = true })
     add_files("metaInfo/metaInfo.cppm")
-    
-    set_values("c++.module.outputdir", "$(buildir)/modules")
--- src/chapter1/xmake.lua
+++ src/chapter1/xmake.lua
@@ -1,12 +1,5 @@
 -- Chapter 1: Array module
-target("chapter1_modules")
-    set_kind("static")
-    set_languages("c++23")
-    
-    set_policy("build.c++.modules", true)
-    
-    add_files("global.cppm")
+target("chapter1_modules")    
+    add_files("global.cppm", { public = true })
     add_files("metaInfo/metaInfo.cppm")
     add_files("array/array.cppm")
-    
-    set_values("c++.module.outputdir", "$(buildir)/modules")
