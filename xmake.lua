a3cddd63622228f575257e09f66778fc0d3dba51
sunrisepeak <speakshen@163.com>
2026-02-06 00:12:20 +0800

    add d2x default config

--- xmake.lua
+++ xmake.lua
@@ -4,6 +4,9 @@ set_project("D2MySTL")
 set_version("0.1.0")
 set_languages("c++23")
 
+add_includedirs(".")
+add_plugindirs("d2x/buildtools")
+
 if is_mode("debug") then
     -- xmake f -m debug
     includes("src/xmake.lua")
