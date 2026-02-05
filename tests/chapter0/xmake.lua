e83b3f48a7f7b27fc7cbfe68835ea779e566805c
sunrisepeak <speakshen@163.com>
2026-02-06 00:07:08 +0800

    update

--- tests/chapter0/xmake.lua
+++ tests/chapter0/xmake.lua
@@ -1,4 +1,5 @@
 target("chapter0_tests")
     add_files("../test_main.cpp")
     add_files("../test_metaInfo.cpp")
+    add_files("../../mystl/chapter0/**.cppm")
     add_deps("chapter0_modules")
--- tests/chapter1/xmake.lua
+++ tests/chapter1/xmake.lua
@@ -4,4 +4,5 @@ target("chapter1_tests")
         "../test_metaInfo.cpp",
         "../test_array.cpp"
     )
+    add_files("../../mystl/chapter1/**.cppm")
     add_deps("chapter1_modules")
